21e9de2ed09e70e695fdb085acfeae412d7f0ccb
Michał Leszczyński <ml@icedev.pl>
2020-12-12 04:24:13 +0100

    Add test for ENCFileData with different meta and file read keys (#15)

--- test_libsdm.py
+++ test_libsdm.py
@@ -43,6 +43,23 @@ def test_sun2():
     assert file_data == b'xxxxxxxxxxxxxxxx'
 
 
+def test_sun3_custom():
+    original_sdmmac_param = config.SDMMAC_PARAM
+    config.SDMMAC_PARAM = ""
+    data_tag, uid, read_ctr_num, file_data = decrypt_sun_message(
+        sdm_meta_read_key=binascii.unhexlify('42aff114f2cb3b6141be6dc95dfc5416'),
+        sdm_file_read_key=binascii.unhexlify('b62a9baf092439bd43c62aee96b970c5'),
+        picc_enc_data=binascii.unhexlify('8ACADDEF0A9B62CDAE39A16B83FC14DE'),
+        sdmmac=binascii.unhexlify('238B2543A8DEBAD8'),
+        enc_file_data=binascii.unhexlify('B8436E11F627BB7F543FCC0C1E0D1A89'))
+    config.SDMMAC_PARAM = original_sdmmac_param
+    
+    assert data_tag == b'\xc7'
+    assert uid == binascii.unhexlify('041d3c8a2d6b80')
+    assert read_ctr_num == 291
+    assert file_data == binascii.unhexlify('4e545858716e6f5f6f42467077792d56')
+
+
 def test_sun2_wrong_sdmmac():
     try:
         original_sdmmac_param = config.SDMMAC_PARAM
@@ -60,8 +77,3 @@ def test_sun2_wrong_sdmmac():
         raise RuntimeError("InvalidSDMMAC was not thrown as expected")
     finally:
         config.SDMMAC_PARAM = original_sdmmac_param
-
-
-test_sun1()
-test_sun2()
-test_sun2_wrong_sdmmac()
