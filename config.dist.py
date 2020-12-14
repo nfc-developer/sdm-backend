3c161c10be07d888710bf42b9652c2bf7e95d0bf
Michał Leszczyński <ml@icedev.pl>
2020-12-14 02:00:16 +0100

    Residual fix after previous PR (#17)

--- config.dist.py
+++ config.dist.py
@@ -1,6 +1,13 @@
 SDM_META_READ_KEY = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 SDM_FILE_READ_KEY = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 
+# for encrypted mirroring
 ENC_PICC_DATA_PARAM = "picc_data"
 ENC_FILE_DATA_PARAM = "enc"
+
+# for plaintext mirroring
+UID_PARAM = "uid"
+CTR_PARAM = "ctr"
+
+# always applied
 SDMMAC_PARAM = "cmac"
